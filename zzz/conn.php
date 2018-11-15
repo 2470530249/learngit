818e0d4c566b124c77f533df6622fbcff6619c05
e <2470530249@qq.com>
2018-11-15 20:07:32 +0800

    op[asdfgghjk!Qw? ;;;;;;;;;;:::::""""""""：ccc

--- zzz/conn.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conn = mysql_connect("localhost","root","1111") or die("数据库连接错误".mysql_error());  
- mysql_select_db("wuliu",$conn) or die("数据库访问错误".mysql_error()); 
- mysql_query("set names utf8");  
-?>
